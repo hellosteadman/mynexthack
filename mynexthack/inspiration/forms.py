e55287a633ea0c7e2ef8c1ed72f86054e5892c86
Mark Steadman <marksteadman@me.com>
2013-09-19 20:36:28 +0100

    Made "What's your idea" box full-width

--- mynexthack/inspiration/forms.py
+++ mynexthack/inspiration/forms.py
@@ -5,7 +5,7 @@ class IdeaForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 		super(IdeaForm, self).__init__(*args, **kwargs)
 		self.fields['title'].widget.attrs['placeholder'] = 'What\'s your idea?'
-		self.fields['title'].widget.attrs['class'] = 'form-control input-large'
+		self.fields['title'].widget.attrs['class'] = 'input-block-level'
 		self.fields['title'].widget.attrs['required'] = self.fields['title'].required and 'required' or ''
 		self.fields['description'].widget.attrs['placeholder'] = 'Give us a bit more detail'
 		self.fields['description'].widget.attrs['class'] = 'input-block-level'
