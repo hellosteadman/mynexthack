dd64c53e751009ccc0c03296ded41a8d2d5d5c2a
Steadman <mark+github@steadman.io>
2014-07-11 13:43:45 +0100

    Updated with Bambu Comments

--- mynexthack/comments/forms.py
+++ mynexthack/comments/forms.py
@@ -1,22 +1,22 @@
 from django import forms
-from bambu.comments.models import Comment
-from bambu.comments.helpers import sanitise
+from bambu_comments.models import Comment
+from bambu_comments.helpers import sanitise
 
 class CommentForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 		super(CommentForm, self).__init__(*args, **kwargs)
 		self.fields['body'].widget.attrs['placeholder'] = 'What\'s your comment?'
 		self.fields['body'].widget.attrs['class'] = 'input-block-level'
-	
+
 	def save(self, commit = True):
 		obj = super(CommentForm, self).save(commit = False)
 		obj.body = sanitise(self.cleaned_data['body'], True)
-		
+
 		if commit:
 			obj.save()
-		
+
 		return obj
-	
+
 	class Meta:
 		model = Comment
 		fields = ('body',)
